1b9fd759a31408260fc640f275201e22aae79418
hishamatf55 <hishamatf50@gmail.com>
2023-07-07 07:14:43 +0300

    upload project

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let icon  = document.querySelector('.icon');
+let nav = document.querySelector('.nav')
+icon.addEventListener('click', ()=>{
+    if(nav.style.display== 'block'){
+        nav.style.display= 'none';
+    }else{
+        nav.style.display= 'block';
+    }
+
+//  icon.style.transform = `rotate(${270}deg`;
+})
+
+const toTop  = document.querySelector('.to-top');
+window.addEventListener('scroll', ()=>{
+    if(window.pageYOffset > 100) {
+        toTop.classList.add('active')
+    }else{
+        toTop.classList.remove('active') 
+    }
+})
